8e89836ba6ea29671d2a35f3f1a5f221427ae071
stefanosechi <stefanosechibusiness@gmail.com>
2025-07-11 22:22:23 +0100

    fix: update import path for Header component in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Header } from "@/components/header"
+import { Header } from "./../components/header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   description:
     "Realizziamo arredamenti su misura  per cucine, soggiorni, camere, uffici e bagni. Falegnameria artigianale di qualità.",
   keywords: "falegnameria, arredamenti su misura, cucine, soggiorni, camere, uffici, bagni",
-    generator: ''
+  generator: ''
 }
 
 export default function RootLayout({
